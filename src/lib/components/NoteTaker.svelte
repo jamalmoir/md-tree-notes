949559c66459178040c36aec8140002ba5bc2816
Jamal Moir <jamal.moir@gmail.com>
2022-03-29 20:35:16 +0900

    Move main components out of index into lib/components
    This commit extracts the main components that was in the main index and puts it in it's own component inside of lib, so that it can be packaged and used externally

---
+++ src/lib/components/NoteTaker.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import ContentEditor from '$lib/components/ContentEditor.svelte'
+	import Sidebar from '$lib/components/Sidebar/Sidebar.svelte'
+	import { nodes, selectedNode } from '$lib/stores';
+	import NodeMenu from '$lib/components/ContextMenu/NodeMenu.svelte';
+
+    let editMode = true
+
+    let sidebarWidth = 250
+    let resizingSidebar = false
+
+    function handleMoveStart(e: MouseEvent) {
+        resizingSidebar = true
+    }
+
+    function handleMoveEnd(e: MouseEvent) {
+        resizingSidebar = false
+    }
+
+    function handleMove(e: MouseEvent) {
+        if (resizingSidebar) {
+            sidebarWidth = e.pageX
+        }
+    }
+</script>
+
+<svelte:body on:mousemove={handleMove} on:mouseup={handleMoveEnd}></svelte:body>
+
+<div class="w-full h-screen">
+    <NodeMenu />
+	<header></header>
+
+	<div class="flex flex-row w-full h-full">
+		<Sidebar {nodes} bind:editMode width={sidebarWidth} />
+		<div
+            class="bg-background h-full w-1 cursor-col-resize"
+            on:mousedown={handleMoveStart}
+        ></div>
+		<div class="w-full h-full">
+			<!-- Editor -->
+			<ContentEditor bind:editMode bind:node={$selectedNode} />
+		</div>
+	</div>
+
+	<footer></footer>
+</div>
